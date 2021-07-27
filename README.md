d4ae93d315d5164c161bd82d83260263de87c009
Daniel Kean <64756591+danielkean@users.noreply.github.com>
2021-07-27 18:57:51 +0100

    Update the theme and implement the ability to transition to the 'add todo' screen

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Kotlin To-Do List Application
 
-This is a to-do list application created using Android, for the purposes of performance testing.
+This is a to-do list application created using Kotlin, for the purposes of performance testing.
 
--- app/src/main/java/com/danielkean/kotlintodoapp/MainActivity.kt
+++ app/src/main/java/com/danielkean/kotlintodoapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.danielkean.kotlintodoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.main, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.action_add_todo -> openAddTodoScreen()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun openAddTodoScreen() {
+
+        Toast.makeText(this,"Add Todo Clicked!", Toast.LENGTH_SHORT).show()
+
+        val intent =  Intent(this, AddTodoActivity::class.java)
+        startActivity(intent)
     }
 }
